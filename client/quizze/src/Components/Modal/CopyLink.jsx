2a0f1ac5b703185ae76cf1625726e06448ea090d
codeloopers <srivastavamayank80@gmail.com>
2024-09-02 02:02:55 +0530

    Update CopyLink.jsx

--- client/quizze/src/Components/Modal/CopyLink.jsx
+++ client/quizze/src/Components/Modal/CopyLink.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const CopyLink = ({ link }) => {
-    const fullUrl = `http://localhost:3000${link}`;
+    const fullUrl = `https://msmayank250-gmail-com-cuvette-final.onrender.com${link}`;
   const copyToClipboard = () => {
     navigator.clipboard.writeText(fullUrl)
       .then(() => alert('Link copied to clipboard!'))
